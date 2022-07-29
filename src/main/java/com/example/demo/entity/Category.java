653545374fa6d194560df8eba4bfc04effa9202c
nilupulheshan <niluheshan@gmail.com>
2022-07-29 12:44:54 +0530

    item & category entity done

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Collection<Item> itemCollection;
+
+    public Category() {
+    }
+
+    
+}
---
+++ src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Asus
+ */
+@Entity
+@Setter
+@Getter
+@Table(name = "item")
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long itemid;
+
+    @Column(name = "av_stock")
+    private Integer avStock;
+
+    @Column(name = "unitPrice")
+    private Long unitPrice;
+
+    @Column(name = "unitCost")
+    private Long unitCost;
+
+    @Column(name = "preOrderMargin")
+    private Integer preOrderMargin;
+
+    @Column(name = "description")
+    private String description;
+
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Category category;
+
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+//    private Collection<OrderDetails> orderDetailsCollection;
+//
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+//    private Collection<Preorder> preorderCollection;
+
+    public Item() {
+    }
+
+    
+}
